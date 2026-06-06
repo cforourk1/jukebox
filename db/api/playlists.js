5f24c249a3b3fdc5de35080c38bcd75a0b65c3bc
Crystal <cforourk@umich.edu>
2026-06-06 15:20:46 -0400

    playlists js routes are in progres. still having a hard time looking at it confidentally but when claude reexplains it to me when I ask clarifying questions, it helps.

---
+++ db/api/playlists.js
@@ -0,0 +1,36 @@
+import express from "express";
+import { createPlaylist, getPlaylists, getPlaylistById } from "#db/queries/playlists";
+
+const router = express.Router();
+export default router;
+
+// router looking for data first before returning
+router.get("/", async (req, res) => {
+  const playlists = await getPlaylists();
+  res.send(playlists);
+});
+
+/* router param is a special express feature that goes and tries to find the playlist from the request. If it is found it will pass it to the next router step. If it is not found it will throw an error. This helps simplify things in terms of time spent on requests
+*/
+router.param("id", async (req, res, next, id) => {
+  const playlist = await getPlaylistById(id);
+  if (!playlist) return res.status(404).send("playlist not found.");
+  req.playlist = playlist;
+  next();
+});
+
+router.get("/:id", (req, res) => {
+  res.send(req.playlist);
+});
+
+router.post("/", async (req, res) => {
+  if (!req.body) return res.status(400).send("Request body required.");
+
+  const { name, description } = req.body;
+  if (!name || !description) {
+    return res.status(400).send("Request body needs: name, description");
+  }
+
+  const playlist = await createPlaylist(name, description);
+  res.status(201).send(playlist);
+});
--- db/api/tracks.js
+++ db/api/tracks.js
@@ -1,18 +1,17 @@
 import express from "express";
+import { getTracks, getTrackById } from "#db/queries/tracks";
+
 const router = express.Router();
 export default router;
 
-import { createTrack } from "#db/queries/tracks";
-import { createPlaylist } from "#db/queries/playlists";
-import { createPlaylistTrack } from "#db/queries/playlists_tracks";
-
-
+// router looking for data first before returning
 
 router.get("/", async (req, res) => {
-  const tracksRouter = await getTracks();
+  const tracks = await getTracks();
   res.send(tracks);
 });
-
+/* router param is a special express feature that goes and tries to find the track from the request. If it is found it will pass it to the next router step. If it is not found it will throw an error. This helps simplify things in terms of time spent on requests
+*/
 router.param("id", async (req, res, next, id) => {
   const track = await getTrackById(id);
   if (!track) return res.status(404).send("Track not found.");
