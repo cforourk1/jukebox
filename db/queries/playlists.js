822f89d98252ef6b94dde2b5da0805bea9d41b67
Crystal <cforourk@umich.edu>
2026-06-06 10:49:40 -0400

    playlists queries file is done. I have a hard time reading the join statements and need to keep making comments so it sticks better.

--- db/queries/playlists.js
+++ db/queries/playlists.js
@@ -1,6 +1,6 @@
 import db from "#db/client";
 
-//create a playlist 
+//create a playlist
 
 export async function createPlaylist(name, description) {
   const sql = `
@@ -14,4 +14,47 @@ export async function createPlaylist(name, description) {
     rows: [playlist],
   } = await db.query(sql, [name, description]);
   return playlist;
+}
+
+// return all playlists 
+
+export async function getPlaylists() {
+  const sql = `
+  SELECT *
+  FROM playlists
+  `;
+  const { rows: playlists } = await db.query(sql);
+  return playlists;
+}
+
+
+// get a playlist by ID
+
+export async function getPlaylistById(id) {
+  const sql = `
+  SELECT *
+  FROM playlists
+  WHERE id = $1
+  `;
+  const {
+    rows: [playlist],
+  } = await db.query(sql, [id]);
+  return playlist;
+}
+
+/* selecting distinct tracks.*  no duplicate rows. tracks can appear accros many playlists. this will remove duplicates. we use tracks.* to signify whic columns we want. we are getting it from the junction table playlists tracks. we are joining on the IDs of the tracks and playlists where they match and then locating the tracs based on the playlist ID and returning those tracks.
+*/
+
+export async function getTracksByPlaylistId(id) {
+  const sql = `
+  SELECT DISTINCT tracks.*
+  FROM
+    playlists_tracks
+    JOIN playlists ON playlists_tracks.playlist_id = playlists.id
+    JOIN tracks ON playlists_tracks.track_id = tracks.id
+  WHERE
+    playlists.id = $1
+  `;
+  const { rows: tracks } = await db.query(sql, [id]);
+  return tracks;
 }
