332788f441b4368a03adf05c09e29f2e480e430d
Crystal <cforourk@umich.edu>
2026-06-06 10:12:12 -0400

    added morgan. I didnt realize that was a package. copied in some information from hospital bridge for the error handling and moving towares building the api files

--- app.js
+++ app.js
@@ -1,3 +1,39 @@
 import express from "express";
+import morgan from "morgan";
+import playlistsRouter from "#api/playlists";
+import tracksRouter from "#api/tracks";
+
 const app = express();
 export default app;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Morgan is logging middleware!
+app.use(morgan("dev"));
+
+app.use("/tracks", tracksRouter);
+app.use("/playlists", playlistsRouter);
+
+// Handles PostgreSQL errors
+app.use((err, req, res, next) => {
+  // Foreign key violation
+  if (err.code === "23503") {
+    return res.status(400).send(err.detail);
+  }
+// invalid syntax
+if (err.code === "22P02") {
+  return res.status(400).send(err.message);
+}
+
+
+
+  next(err);
+});
+
+
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("Sorry! Something went wrong.");
+});
