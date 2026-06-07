b386e915e7b726328ba33ca0e6d0b218a7669b38
Crystal <cforourk@umich.edu>
2026-06-07 07:22:20 -0400

    i had a file structure problem and had to fix imports in package json

--- api/playlists.js
+++ api/playlists.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { createPlaylist, getPlaylists, getPlaylistById } from "#db/queries/playlists";
+import { getTracksByPlaylistId } from "#db/queries/playlists";
+import { createPlaylistTrack } from "#db/queries/playlists_tracks";
 
 const router = express.Router();
 export default router;
@@ -14,7 +16,7 @@ router.get("/", async (req, res) => {
 */
 router.param("id", async (req, res, next, id) => {
   const playlist = await getPlaylistById(id);
-  if (!playlist) return res.status(404).send("playlist not found.");
+  if (!playlist) return res.status(404).send("Playlist not found.");
   req.playlist = playlist;
   next();
 });
@@ -25,12 +27,25 @@ router.get("/:id", (req, res) => {
 
 router.post("/", async (req, res) => {
   if (!req.body) return res.status(400).send("Request body required.");
-
   const { name, description } = req.body;
   if (!name || !description) {
     return res.status(400).send("Request body needs: name, description");
   }
-
   const playlist = await createPlaylist(name, description);
   res.status(201).send(playlist);
+});
+
+// get all tracks in a playlist
+router.get("/:id/tracks", async (req, res) => {
+  const tracks = await getTracksByPlaylistId(req.playlist.id);
+  res.send(tracks);
+});
+
+// add a track to a playlist
+router.post("/:id/tracks", async (req, res) => {
+  if (!req.body) return res.status(400).send("Request body required.");
+  const { trackId } = req.body;
+  if (!trackId) return res.status(400).send("trackId is required.");
+  const playlistTrack = await createPlaylistTrack(req.playlist.id, trackId);
+  res.status(201).send(playlistTrack);
 });
--- app.js
+++ app.js
@@ -15,21 +15,21 @@ app.use(morgan("dev"));
 app.use("/tracks", tracksRouter);
 app.use("/playlists", playlistsRouter);
 
+
 // Handles PostgreSQL errors
 app.use((err, req, res, next) => {
-  // Foreign key violation
+    // Foreign key violation
   if (err.code === "23503") {
     return res.status(400).send(err.detail);
   }
-// invalid syntax
-if (err.code === "22P02") {
-  return res.status(400).send(err.message);
-}
-
-
-
+  // invalid syntax
+  if (err.code === "22P02") {
+    return res.status(400).send(err.message);
+  }
   next(err);
 });
+
+
 
 
 /* catch all error handler that sends a message if none of the other error handlers can give a specific message
